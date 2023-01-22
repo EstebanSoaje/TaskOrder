5315236ab45f27f113228dea85c7ba39bac3d082
Esteban S. F <89812280+EstebanSoaje@users.noreply.github.com>
2023-01-22 11:01:39 -0300

    modificaciones

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # TaskOrder
 App web para ordenar tus tareas por prioridad.
+
+https://www.freshworks.com/latam/freshservice/matriz-de-prioridades/
+
+https://soporteycia.com/sites/default/files/boletin-prioridades.pdf
+
--- prog.js
+++ prog.js
@@ -58,3 +58,88 @@ Lógica:
 Puntaje Total = Gravedad x Urgencia x Tendencia
 
 */
+
+/*
+Gestion de tareas
+Importante y urgente:
+- incidencias fuertes
+- problemas con clientes clave (vips)
+- problemas que impliquen dinero
+- problemas legales
+
+Importantes:
+- Proyectos trimestrales
+- 
+
+Urgentes:
+- incidencias 
+- procesos administrativos
+- consultas o comentarios
+
+Desechables:
+- Proyectos fuera de foco
+- 
+
+*/
+const $select = document.querySelector("#clientes");
+
+
+
+const opcionCambiada = () => {
+    console.log("Cambio");
+};
+
+$select.addEventListener("change", opcionCambiada);
+
+const agregar = () => {
+    const option = document.createElement('option');
+    option.value = document.getElementById("numeroCliente");
+    option.text = document.getElementById("nombreCliente");
+    $select.appendChild(option);
+};
+
+const limpiar = () => {
+    for (let i = $select.options.length; i >= 0; i--) {
+        $select.remove(i);
+    }
+};
+
+const mostrar = () => {
+    const indice = $select.selectedIndex;
+    if (indice === -1) return; // Esto es cuando no hay elementos
+    const opcionSeleccionada = $select.options[indice];
+    alert(`Texto: ${opcionSeleccionada.text}. Valor: ${opcionSeleccionada.value}`);
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelector("#btnAgregar").addEventListener("click", agregar);
+    document.querySelector("#btnLimpiar").addEventListener("click", limpiar);
+    document.querySelector("#btnMostrar").addEventListener("click", mostrar);
+});
+
+
+
+class Tarea {
+    constructor(titulo, fechaEntrega, prioridad, cliente) {
+        this.titulo = titulo;
+        this.fechaEntrega = fechaEntrega;
+        this.prioridad = prioridad;
+        this.cliente = cliente;
+    }
+}
+
+class Cliente {
+    nombre = String;
+    esVip = Boolean;
+}
+
+function agregarTarea() {
+    let tarea = new Tarea(document.getElementById("taskImput"), document.getElementById("fechaEntrega"), establecerPrioridad(), document.getElementById("cliente"))
+}
+
+function establecerPrioridad() {
+    //toma los datos marcados en el formulario y define un puntaje para la tarea
+    let puntaje = 1;
+    return puntaje;
+}
+
